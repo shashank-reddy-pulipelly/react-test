6b81a18a0788c365c950c8a48a7ef4620e5e2081
shashank reddy <shashankpuli123@gmail.com>
2020-10-11 15:58:20 +0530

    Updates

---
+++ precache-manifest.5558fc93c1172f4373988bf077550854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc5a04d0b1b47285d0190d2f39e601e",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "426641b43c9c708c2ca8",
+    "url": "/react-test/static/css/main.2f4705df.chunk.css"
+  },
+  {
+    "revision": "5cb7aaa68a8f48d962c4",
+    "url": "/react-test/static/js/2.353d01c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-test/static/js/2.353d01c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426641b43c9c708c2ca8",
+    "url": "/react-test/static/js/main.74db5c7a.chunk.js"
+  },
+  {
+    "revision": "67d21d5eedc60e1d8cb3",
+    "url": "/react-test/static/js/runtime-main.bb426ec1.js"
+  }
+]);
